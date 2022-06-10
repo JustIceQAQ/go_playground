fbad1641c34943bbbef647909219fa48e858d0ea
justiceqaq <justiceqaq@gmail.com>
2022-06-10 17:34:25 +0800

    refactor: exp_calculator.go

--- exp_calculator.go
+++ exp_calculator.go
@@ -5,35 +5,26 @@ import (
 )
 import UserInputStruct "go_playground/exp_calculator_utils"
 
-func addition(a int, b int) int {
-	return a + b
-}
-func subtraction(a int, b int) int {
-	return a - b
-}
-func multiplication(a int, b int) int {
-	return a * b
-}
-
-func division(a int, b int) int {
-	return a / b
-}
-
 func formatPrint(icon string, r int) string {
 	return fmt.Sprint("a ", icon, " b = ", r)
 }
 
 func doAllCalculator(userInput UserInputStruct.UserInput) {
-	fmt.Println(formatPrint("+", addition(userInput.A, userInput.B)))
-	fmt.Println(formatPrint("-", subtraction(userInput.A, userInput.B)))
-	fmt.Println(formatPrint("*", multiplication(userInput.A, userInput.B)))
-	fmt.Println(formatPrint("/", division(userInput.A, userInput.B)))
+	fmt.Println(formatPrint("+", userInput.Addition()))
+	fmt.Println(formatPrint("-", userInput.Subtraction()))
+	fmt.Println(formatPrint("*", userInput.Multiplication()))
+	fmt.Println(formatPrint("/", userInput.Division()))
 }
 
 func main() {
 	userInput := UserInputStruct.UserInput{}
 	fmt.Println("Calculator")
 	fmt.Println("Pls input a and b value: ")
-	fmt.Scanln(&userInput.A, &userInput.B) // 50 5
+	_, err := fmt.Scanln(&userInput.A, &userInput.B)
+	if err == nil {
+
+		return
+	} // 50 5
+	userInput.ShowValue()
 	doAllCalculator(userInput)
 }
--- exp_calculator_utils/exp_calculator_class.go
+++ exp_calculator_utils/exp_calculator_class.go
@@ -1,6 +1,25 @@
 package exp_calculator_utils
 
+import "fmt"
+
 type UserInput struct {
 	A int
 	B int
+}
+
+func (u *UserInput) ShowValue() {
+	fmt.Println(fmt.Sprint("A = ", u.A, ", B = ", u.B))
+}
+
+func (u *UserInput) Addition() int {
+	return u.A + u.B
+}
+func (u *UserInput) Subtraction() int {
+	return u.A - u.B
+}
+func (u *UserInput) Multiplication() int {
+	return u.A * u.B
+}
+func (u *UserInput) Division() int {
+	return u.A / u.B
 }
